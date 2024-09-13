e80ac8ed0615c44b999a98eb7e215bf564fb69ea
jean-guiollot <jean.guiollot@edu.esiee.fr>
2024-09-13 07:02:26 +0000

    10/10

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
+'''
+cette fonction sert a conaitre les nombre premier
+'''
 from math import sqrt
 
-#### Fonction secondaire
 
 
 def isprime(p):
-    i = 2
-    while i < p and p % i != 0:
-        i = i + 1
-    if i == p :
-        print(p ,"est un nombre premier")
-    
+    '''
+    retoourne un boléin qui indique et indique si ce nombre est premier ou non
 
-    pass
+    arg p valeur entière positive
+
+    return boléin objectif true false
+    '''
+    for k in range(2,int(sqrt(p)+1)) :
+        if p%k == 0 :
+            return False
+    return True
+
 
 #### Fonction principale
 
 
 def main():
+    '''
+    test de la fonction jusqu'a 99
 
+    arg :
+        None
     
-
+    return :
+        None
+    '''
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
